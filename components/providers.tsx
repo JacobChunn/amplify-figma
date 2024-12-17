c5f9d19288ee9b544d8a3a77e8eee9fede675d82
Jacob Chunn <chunnjake@gmail.com>
2024-12-17 01:24:25 -0500

    added initial branch

---
+++ components/providers.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import {Amplify} from 'aws-amplify';
+import { PropsWithChildren } from 'react';
+
+Amplify.configure({
+    
+})
+
+export default function Providers({children}: PropsWithChildren) {
+  return <>{children}</>
+}
